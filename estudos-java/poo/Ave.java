bf3a8991a228930a8bf537f76f1327518a3391e9
Nath <108048568+devmend3s@users.noreply.github.com>
2024-02-21 22:49:52 -0300

    Update Ave.java

--- estudos-java/poo/Ave.java
+++ estudos-java/poo/Ave.java
@@ -10,7 +10,25 @@ public class Ave {
 	String especie;
 	
 	//metodos
+	Ave (int peso, int altura, int envergadura, String nomePopular, String genero, String especie){
+		this.peso = peso;				// g
+		this.altura = altura;			// cm
+		this.envergadura = envergadura;	// cm	
+		this.nomePopular = nomePopular;
+		this.genero = genero;
+		this.especie = especie;
+	}
 	public String Cantar() {
 		return "Fiu, Fiu, Fiu";
 	}
-}
+
+	public void MostrarNomes(){
+		System.out.println("Peso:" +peso);
+		System.out.println("altura:" +altura);
+		System.out.println("nomePopular:" +nomePopular);
+		System.out.println("genero:" +genero);
+		System.out.println("invergadura:" +invergadura);
+		System.out.println("especie:" +especie);
+	}
+
+} 
